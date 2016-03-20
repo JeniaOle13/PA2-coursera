ef0dc0967ca9aefaa5d421297599d1d2b8714ae1
Acari <jeniaole13@mail.ru>
2016-03-20 19:21:48 +0300

    Add last version

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+    m.in <- NULL
+    set <- function(y) {
+        x <<- y
+        m.in <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(Inverse) m.in <<- Inverse
+    getinverse <- function() m.in
+    list(set = set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
+}
 
 ## Write a short comment describing this function
 
-
-makeCacheMatrix <- function(x = numeric()) {
-    cache <- NULL
-    setMatrix <- function(newValue) {
-        x <<- newValue
-        cache <<- NULL
-    }
-    getMatrix <- function() {
-        x
-    }
-    cacheInverse <- function(solve) {
-        cache <<- solve
-    }
-    getInverse <- function() {
-        cache
-    }
-    list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
-}
-
-cacheSolve <- function(y, ...) {
-    inverse <- y$getInverse()
-    if(!is.null(inverse)) {
+cacheSolve <- function(x, ...) {
+    m.in <- x$getinverse()
+    if(!is.null(m.in)) {
         message("getting cached data")
-        return(inverse)
+        return(m.in)
     }
-    data <- y$getMatrix()
-    inverse <- solve(data)
-    y$cacheInverse(inverse)
-    inverse
+    data <- x$get()
+    m.in <- pseudoinverse(data, ...)
+    x$setinverse(m.in)
+    m.in
 }
+
+############ 
+a <- matrix(1:10, nrow = 10, ncol = 20)
+i.a <- makeCacheMatrix(a)
+i.a$get()
+cacheSolve(i.a)
+in.a <- cacheSolve(i.a)
+in.a %*% a
